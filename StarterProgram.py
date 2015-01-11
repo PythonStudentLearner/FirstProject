780cb75029c77778e4bbcbe7baee5391f29d65f6
pythonstudentlearner <pythondtudentlearner@outlook.com>
2015-01-11 17:04:05 +0000

    restful end points

--- StarterProgram.py
+++ StarterProgram.py
@@ -1,8 +1,33 @@
+#!/usr/bin/env python
+import web
+import xml.etree.ElementTree as ET
 
-a = 3
-print (a)
-b = 4
-z = a + b
-print (b)
-print (z)
+tree = ET.parse('user_data.xml')
+root = tree.getroot()
 
+urls = (
+    '/users', 'list_users',
+    '/users/(.*)', 'get_user'
+)
+
+app = web.application(urls, globals())
+
+print("booting up classes")
+class list_users:
+    def GET(self):
+	output = 'users:[';
+	for child in root:
+                print 'child', child.tag, child.attrib
+                output += str(child.attrib) + ','
+	output += ']';
+        return output
+
+
+class get_user:
+    def GET(self, user):
+	for child in root:
+		if child.attrib['id'] == user:
+		    return str(child.attrib)
+
+if _name_ == "_main_":
+    app.run()
